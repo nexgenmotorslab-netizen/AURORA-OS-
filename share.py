42cbf334c9307d1106b35cab862a8be377e72d3b
nexgenmotorslab-netizen <nexgenmotorslab@gmail.com>
2026-08-25 20:48:51 +0000

    Delete share.py

--- share.py
+++
@@ -1,6 +0,0 @@
-from pyngrok import ngrok
-
-print("Sharing AURORA OS...")
-url = ngrok.connect(8000)
-print(f"SEND THIS LINK TO COLLECTORS: {url}")
-input("Press Enter to keep sharing... keep this windows open!")
